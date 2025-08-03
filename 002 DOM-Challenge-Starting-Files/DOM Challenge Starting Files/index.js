fb8a2bf6b2304157a6d14c57e0ed93b17ee9db15
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-03 18:26:32 +0100

    Give the h1 a class list of huge

--- 002 DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
+++ 002 DOM-Challenge-Starting-Files/DOM Challenge Starting Files/index.js
@@ -3,3 +3,5 @@ const thirdLi = ul.children[2];
 thirdLi.innerHTML = "Daniel";
 
 document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
