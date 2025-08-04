335135d847815b699aed06dbac7741474c12122b
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-04 22:42:44 +0100

    Change the text color of the button that got clicked to white

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,8 @@ let button = document.querySelectorAll(".drum");
 
 button.forEach((button) => {
   button.addEventListener("click", function () {
-    const sound = new Audio("./sounds/tom-1.mp3");
-    sound.play();
+    this.style.color = "white";
+    // const sound = new Audio("./sounds/tom-1.mp3");
+    // sound.play();
   });
 });
