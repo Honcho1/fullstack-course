3d2a5cf14ce605f75cfd79bb19754158b88dc67a
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-04 09:27:22 +0100

    Use a random number to pick a random image for player2

--- 002 Dicee-Challenge-Starting-Files/index.js
+++ 002 Dicee-Challenge-Starting-Files/index.js
@@ -1,6 +1,9 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
 let diceImage1 = `./images/dice${randomNumber1}.png`;
-
 let player1dice = document.querySelector(".img1");
 player1dice.setAttribute("src", diceImage1);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let diceImage2 = `./images/dice${randomNumber2}.png`;
+let player2dice = document.querySelector(".img2");
+player2dice.setAttribute("src", diceImage2);
