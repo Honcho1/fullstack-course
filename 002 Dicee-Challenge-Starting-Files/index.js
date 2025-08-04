c476a020dc5a7f1439e5b5f27403e6e2cafa2100
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-04 09:23:20 +0100

    Change the selector to the correct class

--- 002 Dicee-Challenge-Starting-Files/index.js
+++ 002 Dicee-Challenge-Starting-Files/index.js
@@ -2,5 +2,5 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 let diceImage1 = `./images/dice${randomNumber1}.png`;
 
-let player1dice = document.querySelector("img1");
+let player1dice = document.querySelector(".img1");
 player1dice.setAttribute("src", diceImage1);
