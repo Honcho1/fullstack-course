cf37cad5d2647ea616b3e6810f29fd9f5bbd1878
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-04 09:18:21 +0100

    Use the random number to pick a random dice image for player1

--- 002 Dicee-Challenge-Starting-Files/index.js
+++ 002 Dicee-Challenge-Starting-Files/index.js
@@ -1,1 +1,6 @@
-const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let diceImage1 = `./images/dice${randomNumber1}.png`;
+
+let player1dice = document.querySelector("img1");
+player1dice.setAttribute("src", diceImage1);
