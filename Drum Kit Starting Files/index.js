7c36e3f6f81088c2d595278f7fd1f571137d2295
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-05 08:10:05 +0100

    Rewrite the function that plays the sound based on the button or key pressed

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,48 +1,91 @@
-let button = document.querySelectorAll(".drum");
+function playSound(key) {
+  switch (key) {
+    case "w":
+      const crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
 
-button.forEach((button) => {
-  button.addEventListener("click", function () {
-    let buttonInnerHTML = this.innerHTML;
+    case "a":
+      const kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
 
-    switch (buttonInnerHTML) {
-      case "w":
-        const crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
+    case "s":
+      const snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
 
-      case "a":
-        const kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
+    case "d":
+      const tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-      case "s":
-        const snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
+    case "j":
+      const tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
 
-      case "d":
-        const tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
+    case "k":
+      const tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
 
-      case "j":
-        const tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
+    case "l":
+      const tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
 
-      case "k":
-        const tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
+    default:
+      console.log(key);
+      break;
+  }
+}
 
-      case "l":
-        const tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
+// let button = document.querySelectorAll(".drum");
 
-      default:
-        console.log(this.innerHTML);
-        break;
-    }
-  });
-});
+// button.forEach((button) => {
+//   button.addEventListener("click", function () {
+//     let buttonInnerHTML = this.innerHTML;
+
+//     switch (buttonInnerHTML) {
+//       case "w":
+//         const crash = new Audio("./sounds/crash.mp3");
+//         crash.play();
+//         break;
+
+//       case "a":
+//         const kick = new Audio("./sounds/kick-bass.mp3");
+//         kick.play();
+//         break;
+
+//       case "s":
+//         const snare = new Audio("./sounds/snare.mp3");
+//         snare.play();
+//         break;
+
+//       case "d":
+//         const tom1 = new Audio("./sounds/tom-1.mp3");
+//         tom1.play();
+//         break;
+
+//       case "j":
+//         const tom2 = new Audio("./sounds/tom-2.mp3");
+//         tom2.play();
+//         break;
+
+//       case "k":
+//         const tom3 = new Audio("./sounds/tom-3.mp3");
+//         tom3.play();
+//         break;
+
+//       case "l":
+//         const tom4 = new Audio("./sounds/tom-4.mp3");
+//         tom4.play();
+//         break;
+
+//       default:
+//         console.log(this.innerHTML);
+//         break;
+//     }
+//   });
+// });
