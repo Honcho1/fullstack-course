779dcc3d85cb4fbf93af9aac874c24f65ca95942
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-10 17:37:06 +0100

    Write the function to get user input using inquirer

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -5,3 +5,32 @@
 */
 
 import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Please enter a URL to generate a QR code:",
+      validate: (input) => input.trim() !== "" || "URL cannot be empty.",
+    },
+    {
+      type: "input",
+      name: "filename",
+      message:
+        "Please enter a filename for the QR code image (without extension):",
+      choices: ["qr_code", "my_qr", "default"],
+    },
+  ])
+  .then((answers) => {
+    const { url, filename } = answers;
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.error("Prompt couldn't be rendered in the current environment.");
+    } else {
+      // Something else went wrong
+      console.error("An error occurred:", error);
+    }
+  });
