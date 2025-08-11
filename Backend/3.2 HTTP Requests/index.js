26f83d09fc928332eb9332992e47180fc072a676
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-11 18:01:01 +0100

    Add more endpoints to the local server

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,7 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello, sire.</h1><p>Welcome to the home page.</p>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1><p>This is the about page.</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send(
+    "<h1>Contact Me</h1><p>Feel free to reach out to me anytime you wish.</p><p>Phone: +2349061175321</p><p>Email: kingdanofficial@gmail.com</p>"
+  );
 });
 
 app.listen(port, () => {
