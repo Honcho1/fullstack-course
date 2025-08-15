0044120b04b5695e512715618596303bd348c86e
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-15 06:24:18 +0100

    Log the form contents to the console when the user clicks the submit button

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -9,10 +9,15 @@ const app = express();
 const port = 3000;
 
 // Mount the middleware using app.use()
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
