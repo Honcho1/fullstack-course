a309a5c4690884244471877f5bdc9eda71062804
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-15 06:14:51 +0100

    Mount the middleware using app.use()

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -8,6 +8,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+// Mount the middleware using app.use()
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
