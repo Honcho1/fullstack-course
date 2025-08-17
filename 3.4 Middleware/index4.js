3f3412bbc80f0aee4f02e6cca609339367fc48ad
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-17 08:50:56 +0100

    Log the request to the console

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, () => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function logger(req, res, next) {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+}
+
+app.listen(PORT, () => {
   console.log(`Listening on port ${port}`);
 });
