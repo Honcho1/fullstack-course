9fbd6c89600f006b0c041db2613033a2271b884e
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-18 16:31:19 +0100

    Add homepage route that shows a different message for weekdays and weekends

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -2,3 +2,18 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const dayIndex = today.getDay();
+
+  const isWeekend = dayIndex === 0 || dayIndex === 6; // 0 = Sunday, 6 = Saturday
+
+  const message = isWeekend
+    ? "Hey! It's the weekend. It's time to have fun!"
+    : "Hey! It's a weekday. It's time to work hard!";
+
+  res.render("index", { message });
+});
