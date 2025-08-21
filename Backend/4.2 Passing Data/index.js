7133a744984cfdbb39a5f2d96f32f84a578d493d
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2025-08-21 22:48:37 +0100

    Tell Express to use the EJS templates

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {});
